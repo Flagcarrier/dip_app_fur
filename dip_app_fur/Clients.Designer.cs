d02c6a7a6cccb1e6dff85a168e8a07920c549ab9
Bread <Bread@DESKTOP-IJK2546>
2018-05-20 12:26:51 +0700

    client design

--- dip_app_fur/Clients.Designer.cs
+++ dip_app_fur/Clients.Designer.cs
@@ -76,6 +76,7 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(606, 43);
             this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             // 
             // label1
             // 
--- dip_app_fur/Clients.cs
+++ dip_app_fur/Clients.cs
@@ -12,6 +12,13 @@ namespace dip_app_fur
 {
     public partial class Clients : Form
     {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
         public Clients()
         {
             InitializeComponent();
@@ -40,6 +47,15 @@ namespace dip_app_fur
         private void button2_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
         }
     }
 }
